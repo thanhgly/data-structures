5aa2f51fb7428d4e2eb3a27b9785b50f51818143
Thanh Ly <thanhgly.se@gmail.com>
2022-09-12 20:42:59 -0700

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    var elToDel = storage['0'];
+    delete storage['0'];
+    _.each(storage, function(value, key) {
+      storage[Number(key) - 1] = value;
+    });
+    delete storage[someInstance.size() - 1];
+    return elToDel;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
+    return value;
   };
 
   someInstance.pop = function() {
+    var elToPop = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return elToPop;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;

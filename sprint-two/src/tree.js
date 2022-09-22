8706a83c72b512a740bcf238615f5bf604add44e
gbmyt <gbmytabc1@gmail.com>
2022-09-21 23:34:28 -0400

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,9 +15,17 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?

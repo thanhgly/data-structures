67469424f8c2ea4172adfd9c595c50a9e0db0985
gbmyt <gbmytabc1@gmail.com>
2022-09-23 23:29:41 -0400

    Added additional working Graph test

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -68,4 +68,12 @@ describe('graph', function() {
     expect(graph.hasEdge(3, 5)).to.equal(true);
     expect(graph.hasEdge(5, 5)).to.equal(true);
   });
+
+  it('should be undirected graph', function() {
+    graph.addNode(4);
+    graph.addNode(9);
+    graph.addEdge(4, 9);
+    expect(graph.hasEdge(4, 9)).to.equal(true);
+    expect(graph.hasEdge(9, 4)).to.equal(true);
+  });
 });
